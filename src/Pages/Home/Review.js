5755a29737afd47286a902b84ba980784afd11ea
SabrinaAfroz1 <sabrina1234091@gmail.com>
2022-05-28 11:46:41 +0600

    add review,show review done

--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -4,10 +4,10 @@ const Review = (props) => {
     const { review } = props;
     return (
         <div className=" ">
-            <div className="card h-96 lg:max-w-lg bg-base-100 shadow-xl ">
+            <div className="card h-48 lg:max-w-lg bg-primary-100 shadow-xl ">
                 <div className="card-body items-center text-center">
                     <h2 className="card-title">{review.name}</h2>
-                    <p>{review.description}</p>
+                    <p>{review.comment}</p>
 
                 </div>
             </div>
--- src/Pages/Home/Reviews.js
+++ src/Pages/Home/Reviews.js
@@ -4,7 +4,7 @@ import Review from './Review';
 const Reviews = () => {
     const [reviews, setReviews] = useState([]);
     useEffect(() => {
-        const url = 'http://localhost:5000/reviews';
+        const url = 'http://localhost:5000/review';
         fetch(url)
             .then(res => res.json())
             .then(data => setReviews(data));
